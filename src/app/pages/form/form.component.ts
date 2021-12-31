f0d7738e72e18167fbe151bb83cf7cd45b0eba58
JR <jrodriguez@queplan.cl>
2021-12-31 15:45:02 -0300

    [Archivo docker]

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
 
